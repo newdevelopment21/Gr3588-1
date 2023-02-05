5995389fd7f941aedf07abae7d6cab0cadbfd315
Nitsetskaya Svetlana Aleksandrovna <newdevelopment.sa44@gmail.com>
2023-02-05 19:06:44 +0300

    решения для заданий 0  1  1_a  2  3  5  7 добавлены

---
+++ Sen1task5/Program.cs
@@ -0,0 +1,19 @@
+﻿string? inputLine = Console.ReadLine();
+if (inputLine != null)
+{
+    int inputNumber = int.Parse(inputLine);
+    int startNumber = inputNumber * (-1);
+    // Входное значение
+    string outLine = string.Empty;
+    while (startNumber<inputNumber)
+{
+    outLine = outLine + startNumber+",";
+    startNumber++;
+    // startNumber = startNumber+1
+
+}
+outLine = outLine+ inputNumber;
+// выводим данные на консоль
+
+Console.WriteLine(outLine);
+}
