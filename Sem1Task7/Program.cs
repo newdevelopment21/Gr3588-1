9a5f378024585ce6a8cf358730beb0b0ba21dbe2
newdevelopment21 <116106176+newdevelopment21@users.noreply.github.com>
2023-02-06 09:55:12 +0300

    Update Program.cs
    ДОБАВИЛА ТЕКСТОВОЕ ОПИСАНИЕ ЗАДАЧЕ 7

--- Sem1Task7/Program.cs
+++ Sem1Task7/Program.cs
@@ -1,8 +1,12 @@
-﻿string? inputLine = Console.ReadLine();
+//домашняя работа , задача 7 sem1Task7 
+// условие: напиши программу которая на вход принимает ТРЕХЗНАЧНОЕ число 
+//и выдает ТОЛЬКО ПОСЛЕДНЮЮ ТРЕТЬЮ ЦЫФРУ ИЗ ЭТОГО ЧИСЛА
+
+string? inputLine = Console.ReadLine();
 if (inputLine != null)
 {
     int inputNumber = int.Parse(inputLine);
-    int lastDigit = inputNumber %10;
+    int lastDigit = inputNumber %10;  // % - МНЕ НУЖЕН ОСТАТОК ОТ ДЕЛЕНИЯ НА 10
     
 Console.WriteLine(lastDigit);
 }
