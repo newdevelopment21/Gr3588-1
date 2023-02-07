692b106fb8613f41a8946c6c96b85b78a4b108c7
Nitsetskaya Svetlana Aleksandrovna <newdevelopment.sa44@gmail.com>
2023-02-07 07:56:26 +0300

    домашняя работа solution for home work added task6

---
+++ Sem1Task6/Program.cs
@@ -0,0 +1,18 @@
+﻿// ДОМАШНЯЯ РАБОТА, ЗАДАЧА №6: напиши программу которая на вход принимает число 
+//и выдает является ли это число четным 
+// т.е делится ли оно рна 2 без остатка
+string? inputLine = Console.ReadLine();
+if (inputLine != null)
+{
+    
+    int inputNum = int.Parse(inputLine);
+
+ if((inputNum%2) == 0) //т.е делится ли оно рна 2 без остатка
+{
+    Console.WriteLine( inputNum + "Да, четное");
+}
+else
+{
+    Console.WriteLine( inputNum + "нет, не четное");
+}
+}
