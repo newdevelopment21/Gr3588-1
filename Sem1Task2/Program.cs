26cfada596a75898128a3d6872e6e9b139802490
Nitsetskaya Svetlana Aleksandrovna <newdevelopment.sa44@gmail.com>
2023-02-07 07:58:38 +0300

    домашняя работа solution for home work added task2

---
+++ Sem1Task2/Program.cs
@@ -0,0 +1,27 @@
+﻿//Домашняя работа
+//задача: напишите программу которая на вход принимает два значения 
+//и выдает какое из них больше. например ф = 5 а = 7 - максимальное 7
+
+
+string? inputNum1 = Console.ReadLine();
+string? inputNum2 = Console.ReadLine();
+
+if(inputNum1 != null && inputNum2 != null)
+{
+int outNumber1 = int.Parse(inputNum1);
+int outNumber2 = int.Parse(inputNum2);
+
+if (outNumber1 == outNumber2)
+{
+    Console.WriteLine("Числа равны");
+}
+
+if (outNumber1 > outNumber2)
+{
+    Console.WriteLine(outNumber1 +  "max");
+}
+else
+{
+    Console.WriteLine(outNumber2 +  "max");
+}
+}
